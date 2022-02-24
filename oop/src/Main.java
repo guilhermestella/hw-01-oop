f112f6b6622d9c4fdaa26e6f0500d4556731760d
Guilherme Augusto Stella <eng.guilhermestella@gmail.com>
2022-02-24 20:05:42 -0300

    clean

--- oop/src/Main.java
+++ oop/src/Main.java
@@ -1,30 +1,8 @@
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.List;
-
 public class Main {
 
     public static void main(String[] args) {
 
-        BigDecimal valorMouse = new BigDecimal("250");
 
-        List<Pagamento> pgtos = new ArrayList<>();
-        Pagamento pgtoCredito = new Credito(valorMouse, 6);
-        Pagamento pgtoTED = new TED(valorMouse);
 
-        pgtos.add(pgtoCredito);
-        pgtos.add(pgtoTED);
-
-        for (Pagamento pgto : pgtos) {
-            if ( pgto instanceof TED ) {
-                ((TED) pgto).cancelar();
-            }
-        }
-
-        BigDecimal sum = BigDecimal.ZERO;
-        for (Pagamento pgto : pgtos) {
-            sum = sum.add(pgto.valor);
-        }
-        System.out.println(sum);
     }
 }
